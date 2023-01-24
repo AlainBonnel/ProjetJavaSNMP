834062f0fde74505babdc86edd2e465eab2886db
Alain Bonnel <alain.bonnel99@gmail.com>
2023-01-24 10:55:20 +0100

    Suppression liste agent dans manager

--- src/Manager.java
+++ src/Manager.java
@@ -5,25 +5,10 @@ import java.util.List;
 public class Manager {
     private String nom;
     private String adresse;
-    private List<AgentImp> agents;
 
     public Manager(String n, String a) throws RemoteException {
         this.nom = n;
         this.adresse = a;
-    }
-
-    public void AjouterAgent(AgentImp a) {
-        agents.add(a);
-    }
-
-    public void RetirerAgent(AgentImp a) {
-        int i = 0;
-        for (AgentImp b : agents) {
-            if (a == b) {
-                agents.remove(i);
-            }
-            i++;
-        }
     }
 
     public String getNom() {
