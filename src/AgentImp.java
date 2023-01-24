57ff2c47396f129ad1203b6beeb37ad480f4fac8
Tom MARTIN <tom.martin.cfdm@gmail.com>
2023-01-24 11:07:19 +0100

    run agent

--- src/AgentImp.java
+++ src/AgentImp.java
@@ -5,7 +5,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class AgentImp extends UnicastRemoteObject implements Agent, Serializable {
+public class AgentImp extends UnicastRemoteObject implements Agent, Serializable,Runnable {
 
     private String nom;
 
@@ -33,7 +33,9 @@ public class AgentImp extends UnicastRemoteObject implements Agent, Serializable
         this.adresse = adresse;
     }
     
-    public static void main(String args[]) throws Exception, MalformedURLException {
+
+	@Override
+	public void run() {
 		// Dï¿½marre le rmiregistry
     	try {
 		LocateRegistry.createRegistry(1099);
@@ -43,8 +45,17 @@ public class AgentImp extends UnicastRemoteObject implements Agent, Serializable
 		// Crï¿½e et installe un gestionnaire de sï¿½curitï¿½
 		// inutile si on ne tï¿½lï¿½charge pas les classes des stubs et parametres
 		// System.setSecurityManager(new RMISecurityManager());
-		AgentImp a = new AgentImp("AgentA", "192.168.10.1");
-		Naming.rebind("A", a);
+		try {
+			AgentImp a = new AgentImp("AgentA", "192.168.10.1");
+			Naming.rebind("A", a);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("A dé½claré auprès du serveur de noms");
 	}
-}
+
+	}
