303d96f261c4fbe686e7f08bbc747df501e5697b
Tom MARTIN <tom.martin.cfdm@gmail.com>
2023-01-30 14:36:02 +0100

    bibliotèque json + test

---
+++ src/MIB/MIBSystem.java
@@ -0,0 +1,29 @@
+package MIB;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+public class MIBSystem {
+   public static void main(String args[]) {
+      JSONParser jsonP = new JSONParser();
+      try {
+         JSONObject jsonO = (JSONObject)jsonP.parse(new FileReader("C:/person.json"));
+     
+         String name = (String) jsonO.get("name");
+         String age = (String) jsonO.get("age");
+         String address = (String) jsonO.get("address");
+         System.out.println("Name :"+ name);
+         System.out.println("Age: "+ age);
+         System.out.println("Address: "+ address);
+      } catch (FileNotFoundException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (ParseException e) {
+         e.printStackTrace();
+      }
+   }
+}
