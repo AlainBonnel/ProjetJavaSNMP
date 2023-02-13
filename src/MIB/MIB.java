a102aa05cb29262c76d39a8aa0458c2e9061a0d6
Tom MARTIN <tom.martin.cfdm@gmail.com>
2023-02-13 15:34:20 +0100

    parse mib pas fini

--- src/MIB/MIB.java
+++ src/MIB/MIB.java
@@ -1,18 +1,37 @@
 package MIB;
 
+import java.io.*;
 import java.util.*;
 
 public class MIB {
-
+	private String Chemin;
 	private HashMap Hashmap;
-	private String[] name;
 	private String[] descr;
+	private String[] name;
 	private String[] adresse;
 	private String[] uptime;
 	
 	
-	MIB(){
-		this.lirefichier();
+	MIB(String chemin) throws IOException{
+		Chemin = chemin;
+		try {
+			FileReader fileReader = new FileReader(Chemin);
+			BufferedReader reader = new BufferedReader (fileReader);
+			String line = reader.readLine();
+		    System.out.println(line);
+		    if (line == "MIB PROJECT SNMP"){
+		    	line = reader.readLine();
+		    	String[] tab = line.split(",");
+		    	name[0] = tab[0];
+		    	name[1] = tab[1];
+		    	String oid = tab[2];
+		    	Hashmap.put(oid,tab);
+		    	
+		            
+		      }} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void lirefichier() {
