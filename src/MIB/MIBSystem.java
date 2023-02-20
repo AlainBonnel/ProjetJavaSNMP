21484e490e90b4e848496411e20e49bff31a3f78
Tom MARTIN <tom.martin.cfdm@gmail.com>
2023-02-20 17:08:32 +0100

    Exemple mib agent

--- src/MIB/MIBSystem.java
+++ src/MIB/MIBSystem.java
@@ -6,21 +6,7 @@ import org.json.simple.parser.ParseException;
 
 public class MIBSystem {
    public static void main(String args[]) {
-	   JSONParser jsonP = new JSONParser();
-	      try {
-	         JSONObject jsonO = (JSONObject)jsonP.parse(new FileReader("C:/person.json"));
-         String name = (String) jsonO.get("name");
-         String age = (String) jsonO.get("age");
-         String address = (String) jsonO.get("address");
-         System.out.println("Name :"+ name);
-         System.out.println("Age: "+ age);
-         System.out.println("Address: "+ address);
-      } catch (FileNotFoundException e) {
-         e.printStackTrace();
-      } catch (IOException e) {
-         e.printStackTrace();
-      } catch (ParseException e) {
-         e.printStackTrace();
-      }
-   }
-}
+	 
+      
+   
+}}
