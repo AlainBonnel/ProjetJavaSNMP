8881bcd52a72ee337b9ad70d92ca3c9da9da6d03
Tom MARTIN <tom.martin.cfdm@gmail.com>
2023-02-27 14:13:15 +0100

    Changement de la hashmap en liste chainé Fonctionne as espected

--- src/MIB/MIBSystem.java
+++ src/MIB/MIBSystem.java
@@ -1,88 +1,62 @@
 package MIB;
 import java.io.*;
-import java.util.HashMap;
+import java.util.*;
 
 
 public class MIBSystem {
 	private HashMap Hashmap;
-   
+    private LinkedList list;
 	public MIBSystem() {
-		
+		this.list = new LinkedList<String>();
 	}
    
    public void init(){
-	   Hashmap.put("1.0.1","SysDescr");
-	   Hashmap.put("1.0.2","SysName");
-	   Hashmap.put("1.0.3","SysUptime");
+	   list.add("SysDescr");
+	   list.add("SysName");
+	   list.add("SysUptime");
 	   
-	   Hashmap.put("2.0.1.1","Manager1Name");
-	   Hashmap.put("2.0.1.2","Manager1MDP");
-	   Hashmap.put("2.0.1.3","Manager1Addr");
-	   Hashmap.put("2.0.1.4","Manager1Abo");
-	   Hashmap.put("2.0.1.5","Manager1Acces");
+	   list.add("Manager1Name");
+	   list.add("Manager1MDP");
+	   list.add("Manager1Addr");
+	   list.add("Manager1Abo");
+	   list.add("Manager1Acces");
 	   
-	   Hashmap.put("2.0.2.1","Manager2Name");
-	   Hashmap.put("2.0.2.2","Manager2Mdp");
-	   Hashmap.put("2.0.2.3","Manager2Addr");
-	   Hashmap.put("2.0.2.4","Manager2Abo");
-	   Hashmap.put("2.0.2.5","Manager2Acces");
+	   list.add("Manager2Name");
+	   list.add("Manager2Mdp");
+	   list.add("Manager2Addr");
+	   list.add("Manager2Abo");
+	   list.add("Manager2Acces");
 	   
-	   Hashmap.put("3.0.1.1","Interface1Descr");
-	   Hashmap.put("3.0.1.2","Interface1Etat");
-	   Hashmap.put("3.0.1.3","Interface1MTU");
-	   Hashmap.put("3.0.1.4","Interface1Addr");
-	   Hashmap.put("3.0.1.5","Interface1Error");
+	   list.add("Interface1Descr");
+	   list.add("Interface1Etat");
+	   list.add("Interface1MTU");
+	   list.add("Interface1Addr");
+	   list.add("Interface1Error");
 	   
-	   Hashmap.put("3.0.2.1","Interface2Descr");
-	   Hashmap.put("3.0.2.2","Interface2Etat");
-	   Hashmap.put("3.0.2.3","Interface2MTU");
-	   Hashmap.put("3.0.2.4","Interface2Addr");
-	   Hashmap.put("3.0.2.5","Interface2Error");
+	   list.add("Interface2Descr");
+	   list.add("Interface2Etat");
+	   list.add("Interface2MTU");
+	   list.add("Interface2Addr");
+	   list.add("Interface2Error");
    }
    
-   public String OidToString(String g){
-	   Object r = "";
-	   r = Hashmap.get(g);
-	   return r.toString();
-   }
-   public String getnextOid(String g) {
-	   String a[] = g.split(".");
-	   String r ="";
-	   for(Object b : Hashmap.keySet()) {
-		   String d = b.toString();
-		   String[] c = d.split(".");
-		   int f = Integer.parseInt(c[1]);
-		   int e = Integer.parseInt(a[1]); 
-		   if (f > e) {
-			   r = d;
-		   }
-		   else if(f == e) {
-			   
-			   int h = Integer.parseInt(c[2]);
-			   int i = Integer.parseInt(a[2]);
-			   if (h > i){
-				   r = d;
+   public String getNext(String i) {
+	   String r ="null";
+	   if(list.contains(i)) {
+		   for(int a=0;a<list.size()-1;a++) {
+			   if(list.get(a) == i) {
+				   r = list.get(a+1).toString();
 			   }
-			   else if(h == i) {
-				   int j = Integer.parseInt(c[3]);
-				   int k = Integer.parseInt(a[3]);
-				   if (j > k) {
-					   r = d;
-				   }
-			   }
-		   }
-		   
-	   }
-	   return r;
-   }
-   public String StringtoOid(String g){
-	   String r ="";
-	   for (Object b : Hashmap.keySet()) {
-		   if (g == Hashmap.get(b)) {
-			   r = b.toString();
 		   }
 	   }
 	   return r;
    }
    
+   public static void main (String[] args){
+	   MIBSystem c = new MIBSystem();
+	   c.init();
+	   System.out.print(c.getNext("Manager2Abo"));
+	   System.out.print(c.getNext("Interface2Error"));
+   }
+  
 }
